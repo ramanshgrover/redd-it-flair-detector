5863678e5abeb975721c124c40a2927e649c4a47
Ramansh Grover <ramanshg29@gmail.com>
2020-04-26 19:22:34 +0530

    changes made

--- app.py
+++ app.py
@@ -15,12 +15,7 @@ from nltk.corpus import stopwords
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    return render_template('index.html')
-
-
-@app.route('/', methods=['POST'])
+@app.route('/')
 def home():
     return render_template('index.html')
 
